3c387c561ba13cd2424487061fe32e4360123325
lucasgerez <lucas.foratto@alumni.usp.br>
2023-11-25 21:35:34 -0300

    incluindo tabelas pnadc

--- scripts/07_PNAD_painel.R
+++ scripts/07_PNAD_painel.R
@@ -84,15 +84,79 @@ pnad <- pnad %>%
          renda_dom_total = VD5007,
          renda_dom_pc = VD5008,
          horas_trabalhadas = VD4013
-         )
+         ) 
+
+pnad <- pnad %>%
+  mutate(setor = case_when( 
+    atividade_estabelecimentos %in% c( 'Alojamento e alimentação', 
+                                    'Serviços domésticos', 
+                                    'Transporte, armazenagem e correio', 
+                                    'Construção', 
+                                    'Educação, saúde humana e serviços sociais', 
+                                    'Informação, comunicação e atividades financeiras, imobiliárias, profissionais e administrativas', 
+                                    'Atividades mal definidas' ) ~ 'Serviços',
+    atividade_estabelecimentos %in% c( 'Comércio, reparação de veículos automotores e motocicletas' ) ~ 'Comércio',
+    atividade_estabelecimentos %in% c( 'Indústria geral') ~ 'Indústria',
+    atividade_estabelecimentos %in% c( 'Agricultura, pecuária, produção florestal, pesca e aquicultura') ~ 'Agricultura',
+    atividade_estabelecimentos %in% c( 'Administração pública, defesa e seguridade social') ~ 'Adm. Pública'
+  )); gc() 
 
 # Passo 2: definir como um objeto do tipo survey
 
-pof_svy <- as_survey(svydesign(ids = ~UPA, 
-                               strata = ~Estrato,
-                               weights = ~peso_dom, 
-                               data = pnad, 
-                               check.strata = TRUE))
+
+# Restringindo a POF para a região de interesse
+
+head(pnad$UF)
+head(pnad$Estrato)
+
+
+# Testes para Curitiba 
+
+pnad_svy <- pnad %>% 
+  filter(UF == 'Paraná',
+         unidade_geo == 'Capital') 
+
+pnad_svy <- as_survey(svydesign(ids = ~UPA, 
+                                strata = ~Estrato,
+                                weights = ~peso_dom, 
+                                data = pnad_svy, 
+                                check.strata = TRUE))
+
+
+# Estatísticas
+
+# Proporção de pessoas ocupadas
+pnad_svy %>%  
+  filter(!is.na(ocupadas)) %>%
+  group_by(ocupadas) %>%
+  summarise(survey_prop()) %>%
+  select(-(ends_with("_se"))) 
+  
+
+
+# Evolução da ocupação e do salário médio por setores na Região Metropolitana 
+
+# Se a gente souber o que constitui o agroalimentar é só acrescentar aqui
+pnad_svy %>% 
+  filter(!is.na(setor)) %>%
+  group_by(setor) %>%
+  summarise(prop_ocupados = survey_prop(),
+            rendimento = survey_mean(renda_trab_princ, na.rm = T)) %>%
+  select(-(ends_with("_se"))) 
+
+
+# Parcipação dos brancos e das mulheres no setor agroalimentar 
+# precisamos saber o que é o setor agroalimentar (pode ser agricultura + alimentos)
+
+pnad_svy %>% 
+  filter(ocupadas == 'Pessoas ocupadas') %>%
+  group_by(sexo) %>%
+  summarise(sexo_ocupados = survey_prop(),
+            sexo_rendimento = survey_mean(renda_trab_princ, na.rm = T)) %>%
+  select(-(ends_with("_se"))) 
+
+
+
 
 
 
